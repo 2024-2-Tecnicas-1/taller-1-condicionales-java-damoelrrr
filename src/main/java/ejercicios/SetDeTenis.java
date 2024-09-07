74ea68f3564f9addba3dd46b9945edfabfa6d7bb
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-09-07 11:03:19 -0500

    Update SetDeTenis.java

--- src/main/java/ejercicios/SetDeTenis.java
+++ src/main/java/ejercicios/SetDeTenis.java
@@ -9,8 +9,28 @@ import java.util.Scanner;
 public class SetDeTenis {
     public static String evaluar(int numVictoriasA, int numVictoriasB) {
         // TODO: Coloca aquí el código del ejercicio 1: Set de tenis
+        if ((numVictoriasA == 3 && numVictoriasB == 7) || (numVictoriasB == 7 && numVictoriasA == 3) || (numVictoriasA == 0 && numVictoriasB == 7) || (numVictoriasB == 0 && numVictoriasA == 7) || 
+            (numVictoriasA == 8 || numVictoriasB == 8) || (numVictoriasA > 7 || numVictoriasB > 7)) {
+            return "Inválido";
+             
+        }
+         if ((numVictoriasA >= 6 && numVictoriasA - numVictoriasB >= 2) || (numVictoriasA==7 && numVictoriasB==6) ) {
+             System.out.println("Ganó A");         
+             
+        }
+        if ((numVictoriasB >= 6 && numVictoriasB - numVictoriasA >= 2)||(numVictoriasA==6 && numVictoriasB==7) ) {
+             System.out.println("Ganó B");  
+             
+        }
+        
+        if ((numVictoriasA < 6 && numVictoriasB < 6) || (numVictoriasA == 5 && numVictoriasB == 5) || (numVictoriasA == 6 && numVictoriasB == 5) || (numVictoriasA == 5 && numVictoriasB == 6) || 
+            (numVictoriasA == 6 && numVictoriasB == 6)) {
+            System.out.println("Aún no termina");
+            
+        }
         return "";
-    }
+    
+}
     
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
@@ -24,3 +44,4 @@ public class SetDeTenis {
         System.out.println(respuesta);
     }
 }
+        
