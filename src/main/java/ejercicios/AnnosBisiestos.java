e95ee5b44b72f3d77ce7903bd8dca2d41ab9b750
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-09-07 12:11:58 -0500

    Update AnnosBisiestos.java
    aca describi calendario juliano y el gregoriano tambien calcule cuales son los los bisiestos antes de 1582 cuando el papa Gregorio XIII lo cambio es decir como seria en calendario gregoriano

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -11,16 +11,56 @@ import java.util.Scanner;
 public class AnnosBisiestos {
     public static String evaluar(int anno){
         // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
+        
+        if (anno <= 1582) {
+            System.out.println("calendario juliano");
+            if (anno % 4 == 0) {
+                System.out.println(anno + " es año bisiesto  ");
+            } else {
+                System.out.println(anno + " no es bisiesto ");
+            }
+        }
+        if (anno >= 1582) {
+            System.out.println("calendario gregoriano ");
+            if (anno % 400 == 0) {
+                return anno + " es un año bisiesto";
+            } else if (anno % 100 == 0) {
+                return anno + " no es bisiesto";
+            } else if (anno % 4 == 0) {
+                return anno + " es un año bisiesto";
+            } else {
+                return anno + " no es bisiesto";
+            }
+
+        }
+        
+        
+        if (anno <= 1582) {
+            System.out.println("caso especial aca calculo cuales son los los bisiestos antes de 1582");
+            System.out.println("calendario gregoriano ");
+            if (anno % 400 == 0) {
+                return anno + " es un año bisiesto";
+            } else if (anno % 100 == 0) {
+                return anno + " no es bisiesto";
+            } else if (anno % 4 == 0) {
+                return anno + " es un año bisiesto";
+            } else {
+                return anno + " no es bisiesto";
+            }
+
+        }
+        
+
         return "";
     }
-    
+
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Año:");
         int anno = lector.nextInt();
-        
+
         String respuesta = evaluar(anno);
         System.out.println(respuesta);
     }
-            
+
 }
