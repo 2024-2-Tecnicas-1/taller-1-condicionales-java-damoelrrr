855d83bd76fd06b74e08482c86deb7a4c7729f29
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-09-07 19:54:01 -0500

    Update AnnosBisiestos.java

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -2,67 +2,37 @@ package javaapplication33;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Daniel Rodriguez
- */
 public class JavaApplication33 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static String evaluar(int anno) {
-        // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-
-        if (anno <= 1582) {
-            System.out.println("calendario juliano");
+        
+        if (anno < 1582) {
             if (anno % 4 == 0) {
-                System.out.println(anno + " es año bisiesto  ");
+                return anno + " es un año bisiesto (calendario juliano)";
             } else {
-                System.out.println(anno + " no es bisiesto ");
+                return anno + " no es bisiesto (calendario juliano)";
             }
-        }
-        if (anno >= 1582) {
-            System.out.println("calendario gregoriano ");
+        } 
+        
+        else {
             if (anno % 400 == 0) {
-                return anno + " es un año bisiesto";
+                return anno + " es un año bisiesto (calendario gregoriano)";
             } else if (anno % 100 == 0) {
-                return anno + " no es bisiesto";
+                return anno + " no es bisiesto (calendario gregoriano)";
             } else if (anno % 4 == 0) {
-                return anno + " es un año bisiesto";
+                return anno + " es un año bisiesto (calendario gregoriano)";
             } else {
-                return anno + " no es bisiesto";
+                return anno + " no es bisiesto (calendario gregoriano)";
             }
-
         }
-
-        if (anno <= 1582) {
-            System.out.println("caso especial aca calculo cuales son los los bisiestos antes de 1582");
-            System.out.println("calendario gregoriano ");
-            if (anno % 400 == 0) {
-                return anno + " es un año bisiesto";
-            } else if (anno % 100 == 0) {
-                return anno + " no es bisiesto";
-            } else if (anno % 4 == 0) {
-                return anno + " es un año bisiesto";
-            } else {
-                return anno + " no es bisiesto";
-            }
-
-        }
-
-        return "";
     }
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        System.out.print("Año:");
+        System.out.print("Año: ");
         int anno = lector.nextInt();
 
         String respuesta = evaluar(anno);
         System.out.println(respuesta);
     }
-
 }
-
-
